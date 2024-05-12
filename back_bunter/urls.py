5e6c1a3d61d64819f4c16d3ac837381881d545c9
unknown <pmoraes230nascimento@gmail.com>
2024-05-12 18:35:16 -0300

    Incio de db no projeto e mudanças na estrurura da pagina de contatos

--- back_bunter/urls.py
+++ back_bunter/urls.py
@@ -21,7 +21,7 @@ from bunter_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('contato', views.contato, name='contato'),
-    path('produtos', views.produto, name='produto'),
-    path('sobre_mim', views.sobre_mim, name='sobre_mim')
+    path('contato/', views.contato, name='contato'),
+    path('produtos/', views.produto, name='produto'),
+    path('sobre_mim/', views.sobre_mim, name='sobre_mim')
 ]
--- bunter_app/views.py
+++ bunter_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Contact
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,14 @@ def produto(request):
     return render(request, 'produtos/produtos.html')
 def sobre_mim(request):
     return render(request, 'sobre_mim/sobre_mim.html')
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST) # type: ignore
+        if form.is_valid():
+            form.save()
+            # Redirecione ou faça algo após o envio bem-sucedido do formulário
+            return redirect('sucesso')
+    else:
+        form = ContactForm() # type: ignore
+    return render(request, 'contato/contatos.html', {'form': form})
