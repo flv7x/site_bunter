02094dc3b7acd1505da7c28cfce1a6cd3ea2e542
unknown <pmoraes230nascimento@gmail.com>
2024-05-14 23:14:59 -0300

    teste db

--- back_bunter/urls.py
+++ back_bunter/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('produtos/', views.produto, name='produto'),
     path('sobre_mim/', views.sobre_mim, name='sobre_mim'),
     path('compra/', views.compra, name='compra'),
-    path('cadastro_cliente/', views.CadastroCliente, name='cadastro_clientes')
+    path('cadastro_cliente/', views.CadastroCliente, name='cadastro_clientes'),
 ]
